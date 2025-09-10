8c028459927dbaff33b83d40c298264cbc3af1e9
Rafal <rafal.hawrylak@gmail.com>
2025-09-10 14:10:56 +0200

    new RandomSingleFullFieldCloseToAnotherFullGlobalGuesser

--- src/main/java/org/hawrylak/puzzle/nonogram/guess/ChosenGuesserProvider.java
+++ src/main/java/org/hawrylak/puzzle/nonogram/guess/ChosenGuesserProvider.java
@@ -2,7 +2,7 @@ package org.hawrylak.puzzle.nonogram.guess;
 
 public class ChosenGuesserProvider implements GuesserProvider {
 
-    private final Guesser guesser = new RandomSingleFullFieldCloseToAnotherFullGuesser();
+    private final Guesser guesser = new RandomSingleFullFieldCloseToAnotherFullGlobalGuesser();
 
     @Override
     public Guesser get() {
